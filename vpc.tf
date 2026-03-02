8a41adb231662ddd3948a1dd892b95a1fd730d7b
satishlakamsani <lakamsanisatish@gmail.com>
2026-03-02 13:54:24 +0530

    added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc"{
-    source = "git::https://github.com/satishlakamsani/terraform-aws-vpc.git?ref=main"
+    # source = "git::https://github.com/satishlakamsani/terraform-aws-vpc.git?ref=main"
+    source = "../terraform-aws-vpc"
     project = var.project
     environment = var.environment
 }
